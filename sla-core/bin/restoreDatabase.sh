4b9efb281a17ccfc408e5ca464bf74560aacb259
segarvi <sergio.garciavillalonga@atosresearch.eu>
2015-06-09 12:37:31 +0200

    More db info.

--- sla-core/bin/restoreDatabase.sh
+++ sla-core/bin/restoreDatabase.sh
@@ -15,5 +15,5 @@ DB=$(get_var "db.name")
 USER=$(get_var "db.username")
 PWD=$(get_var "db.password")
 HOST=$(get_var "db.host")
-echo "Cleaning database: DB='$DB' USER='$USER' HOST='$HOST'"
+echo "Cleaning database: DB='$DB' USER='$USER' HOST='$HOST' PWD='$PWD'"
 mysql -p"$PWD" -u "$USER" -h "$HOST" "$DB" < sla-repository/src/main/resources/sql/atossla.sql
