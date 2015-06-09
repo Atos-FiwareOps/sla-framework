840f442e6d56a822fa94aeaca20413345764f5b8
segarvi <sergio.garciavillalonga@atosresearch.eu>
2015-06-09 12:26:24 +0200

    Parametrizable configuration file.

--- sla-core/bin/autoconfigure.sh
+++ sla-core/bin/autoconfigure.sh
@@ -12,4 +12,6 @@ sed 's/{sla_log_slaatos_fullpathFilename}/'$sla_log_slaatos_fullpathFilename'/g'
 sed 's/{sla_log_thirdpartysw_fullpathFilename}/'$sla_log_thirdpartysw_fullpathFilename'/g' configuration.properties -i
 sed 's/{sla_service_basicsecurity_password}/'$sla_service_basicsecurity_password'/g' configuration.properties -i
 
+echo "Autoconfiguring executed successfully"
+
 exit 0
