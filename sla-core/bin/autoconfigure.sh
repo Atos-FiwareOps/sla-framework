d7c5a944d498a3808ef802c02fb3f71e2ad98bf6
segarvi <sergio.garciavillalonga@atosresearch.eu>
2015-07-31 14:08:34 +0200

    Add the new parameters in the autoconfiguration script
    In the previous commit, only the configuration file was parametrized.
    When adding new parameters in that file, it is also needed to update
    the autoconfiguration script.
    
    In the configuration.properties.sample class, the parameters for the
    enforcement.constraintEvaluator.class and the
    enforcement.metricsRetriever.class were parametrized, but the sed
    commands in the autoconfigure.sh script were not updated to get their
    values from the global variables.

--- sla-core/bin/autoconfigure.sh
+++ sla-core/bin/autoconfigure.sh
@@ -8,6 +8,8 @@ sed 's/{sla_db_host}/'$sla_db_host'/g' configuration.properties -i
 sed 's/{sla_log_slaatos_fullpathFilename}/'$sla_log_slaatos_fullpathFilename'/g' configuration.properties -i
 sed 's/{sla_log_thirdpartysw_fullpathFilename}/'$sla_log_thirdpartysw_fullpathFilename'/g' configuration.properties -i
 sed 's/{sla_service_basicsecurity_password}/'$sla_service_basicsecurity_password'/g' configuration.properties -i
+sed 's/{sla_enforcement_constraintEvaluator_class}/'$sla_enforcement_constraintEvaluator_class'/g' configuration.properties -i
+sed 's/{sla_enforcement_metricsRetriever_class}/'$sla_enforcement_metricsRetriever_class'/g' configuration.properties -i
 
 echo "Autoconfiguring executed successfully"
 
