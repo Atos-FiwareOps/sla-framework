56eaa79afba013ca0376706e16de4a7327f1381c
segarvi <sergio.garciavillalonga@atosresearch.eu>
2015-08-10 10:42:58 +0200

    Corrected a parameter in the autoconfigure.sh file
    The parameter sla_tomcat_directory had no closing brackets. It could
    lead to a wrong substitution when running the autoconfigure script.

--- sla-core/bin/autoconfigure.sh
+++ sla-core/bin/autoconfigure.sh
@@ -2,7 +2,7 @@
 
 cp configuration.properties.sample configuration.properties
 
-sed 's/{sla_tomcat_directory/'$sla_tomcat_directory'/g' configuration.properties -i
+sed 's/{sla_tomcat_directory}/'$sla_tomcat_directory'/g' configuration.properties -i
 sed 's/{sla_db_password}/'$sla_db_password'/g' configuration.properties -i
 sed 's/{sla_db_host}/'$sla_db_host'/g' configuration.properties -i
 sed 's/{sla_log_slaatos_fullpathFilename}/'$sla_log_slaatos_fullpathFilename'/g' configuration.properties -i
